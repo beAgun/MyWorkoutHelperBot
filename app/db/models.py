ee044c81beb1f9ea8501a4100f9cdf6aae2c9bd1
Anastasia <nbegun_314@mail.ru>
2026-06-12 20:51:16 +0300

    fix username

--- app/db/models.py
+++ app/db/models.py
@@ -22,11 +22,11 @@ class User(Base):
 
     chat_id: Mapped[int] = mapped_column(BigInteger, unique=True, nullable=False)
 
-    username: Mapped[str] = mapped_column(
-        String(length=64), unique=True, nullable=False
-    )
+    first_name: Mapped[str | None] = mapped_column(String(length=64), nullable=False)
 
-    first_name: Mapped[str | None] = mapped_column(String(length=64), default=None)
+    username: Mapped[str] = mapped_column(
+        String(length=64), unique=True, nullable=True, default=None
+    )
 
     last_name: Mapped[str | None] = mapped_column(String(length=64), default=None)
 
